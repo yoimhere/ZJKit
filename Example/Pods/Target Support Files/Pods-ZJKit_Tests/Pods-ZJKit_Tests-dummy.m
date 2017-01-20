383ce3dd54c4defcc2e8280f24589900bc0a2f44
yuzhijie <yuzhijie@wifibanlv.com>
2017-01-20 10:40:29 +0800

    Initial Commit of Library
    Signed-off-by: yuzhijie <yuzhijie@wifibanlv.com>

---
+++ Example/Pods/Target Support Files/Pods-ZJKit_Tests/Pods-ZJKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZJKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZJKit_Tests
+@end
---
+++ Example/Pods/Target Support Files/Pods-ZJKit_Tests/Pods-ZJKit_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_ZJKit_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ZJKit_TestsVersionString[];
+
